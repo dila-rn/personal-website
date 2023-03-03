8d796a938b57761c7543f56e53bd46b1a345eb11
Dila Rizvina <dila.rn24@gmail.com>
2023-03-03 18:29:48 +0700

    fix typos

--- src/js/experiences.js
+++ src/js/experiences.js
@@ -6,7 +6,7 @@ const experiences = [
       duration: "Jul 2022 - Feb 2023",
       description: {
         0: "SYNRGY Academy is a collaboration program between BCA and Binar Academy in the form of online bootcamp scholarship.",
-        1: "uring the six months of intensive online bootcamp, with project- based learning method, I learned about basic full- stack web development concepts such as responsive web design, object- oriented programming in JavaScript, DOM manipulation, building RESTful APIs and HTTP servers, creating single-page web applications, and projects deployment.",
+        1: "During the six months of intensive online bootcamp, with project- based learning method, I learned about basic full- stack web development concepts such as responsive web design, object- oriented programming in JavaScript, DOM manipulation, building RESTful APIs and HTTP servers, creating single-page web applications, and projects deployment.",
         2: "Worked on final project with 18 team members and given the opportunity to be the Project Manager",
         3:"Throughout the bootcamp, I enhanced my skills in HTML, CSS, JavaScript, Bootstrap, React.js, Next.js, Node.js, Express.js, Sequelize, Railway, Vercel, CI/CD and Git."
       }
@@ -18,7 +18,7 @@ const experiences = [
       role: "Graphic Designer",
       duration: "Apr 2022 - Jun 2022",
       description: {
-        0: "As a graphic designer for LUVIA Hijab, I successfully created stunning designs for the LUVIA and LUVKID brands, including graphics for social media promotion, catalogues, and merchandise, using my expertise in Adobe Photoshop and Illustrator",
+        0: "As a graphic designer for LUVIA Hijab, I successfully created stunning designs for the LUVIA and LUVKID brands, including graphics for social media promotion, catalogues, and merchandise, using my expertise in Adobe Photoshop and Illustrator.",
         1: "In addition to my design work, I also particiated in product photography, photo editing, and video editing for LUVIA's social media content, ensuring that the brand's visual identity remained consistent and captivating to its audience."
       }
     },
@@ -39,8 +39,8 @@ const experiences = [
       role: "Laboratory Assistant of Digital and Microprocessor",
       duration: "Oct 2020 - Dec 2020",
       description: {
-        0: "I was responsible for assisting the students to do lab works and simulations, such as Combinatorial Logic circuit, Digital Aritmethic, Digital I/O Microprocessor, and Analog I/O Microprocessor, all while honing my own skills with ESP32 Microcontrollers and Autodesk Tinkercad",
-        1: "Working with students on these critical concepts not only deepened my own understanding of these technologies, but also helped me to develop my abilities as an educator, enabling me to communicate complex ideas with clarity and confidence"
+        0: "I was responsible for assisting the students to do lab works and simulations, such as Combinatorial Logic circuit, Digital Aritmethic, Digital I/O Microprocessor, and Analog I/O Microprocessor, all while honing my own skills with ESP32 Microcontrollers and Autodesk Tinkercad.",
+        1: "Working with students on these critical concepts not only deepened my own understanding of these technologies, but also helped me to develop my abilities as an educator, enabling me to communicate complex ideas with clarity and confidence."
       }
     },
   ]
